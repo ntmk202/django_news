b48c3f6c39eb8985cbb163821a798bc32771f807
nguyen minh khue <ntmkhue.20it6@vku.udn.vn>
2023-11-11 09:04:16 +0700

    change cm

--- django_news/settings.py
+++ django_news/settings.py
@@ -166,7 +166,7 @@ LOCALE_PATHS = [
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [ 
     BASE_DIR / "static",
     ]
