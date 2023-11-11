ec400952a308d190b5efdee1df70fb46c87c69f9
nguyen minh khue <ntmkhue.20it6@vku.udn.vn>
2023-11-11 10:17:58 +0700

    change cm

--- django_news/settings.py
+++ django_news/settings.py
@@ -167,13 +167,14 @@ LOCALE_PATHS = [
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [ 
-    BASE_DIR / "static",
-    ]
+# STATICFILES_DIRS = [ 
+#     BASE_DIR / "static",
+#     ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 #MEDIA
-MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR / "media/"
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
